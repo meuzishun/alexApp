e21f4a5aa45a73a2c6a9da9b454bf5fd242b3500
Andrew Smith <meuzishun@gmail.com>
2022-02-06 15:17:55 -0500

    calc working

--- futureValue.js
+++ futureValue.js
@@ -3,37 +3,38 @@ const resultContainer = document.querySelector('#future-value-result');
 // console.log(form);
 form.addEventListener('submit', handleCalcBtn);
 
-function getFutureValue(pmt, int, compPer) {
-	const val = Math.pow(1 + int, compPer);
-	const futureValue = pmt * ((val - 1) / int);
-	return futureValue;
+function getFutureValue(presVal, pmt, int, compPer) {
+  let futureValue = presVal;
+  for (let i = 0; i < compPer; i++) {
+    futureValue = (futureValue + pmt) * (1 + int);
+  }
+  return futureValue;
 }
 
 function roundToTwoDec(num) {
-	return Math.round(num * 100) / 100;
+  return Math.round(num * 100) / 100;
 }
 
 function gatherInputs(form) {
-	const inputs = [...form.querySelectorAll('input')];
-	const values = {};
-	inputs.forEach((input) => {
-		values[input.id] = Number(input.value);
-	});
-	return values;
+  const inputs = [...form.querySelectorAll('input')];
+  const values = {};
+  inputs.forEach((input) => {
+    values[input.id] = Number(input.value);
+  });
+  return values;
 }
 
 function handleCalcBtn(e) {
-	e.preventDefault();
-	const form = e.srcElement;
-	const values = gatherInputs(form);
-	// console.log(values);
-	const futureValue = getFutureValue(values.payment, values.interest, values.periods);
-	const result = roundToTwoDec(futureValue);
-	// console.log(result);
-	resultContainer.textContent = `$${result}`;
+  e.preventDefault();
+  const form = e.srcElement;
+  const values = gatherInputs(form);
+  console.log(values);
+  const futureValue = getFutureValue(
+    values.present_value,
+    values.payment,
+    values.interest,
+    values.periods
+  );
+  const result = roundToTwoDec(futureValue);
+  resultContainer.textContent = `$${result}`;
 }
-
-// console.log(roundToTwoDec(getFutureValue(25, 0.09, 3)));
-// const futureValue = getFutureValue(25, 0.09, 3);
-// const roundedFutureValue = roundToTwoDec(futureValue);
-// console.log(roundedFutureValue);
